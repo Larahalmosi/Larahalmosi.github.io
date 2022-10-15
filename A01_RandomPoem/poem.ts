8c74592b34b5f0ecc9cf174e88b9f3effea46475
@y0urv39et4bles <101884905+Larahalmosi@users.noreply.github.com>
2022-10-15 14:00:36 +0200

    oo1

---
+++ A01_RandomPoem/poem.ts
@@ -0,0 +1,17 @@
+/*
+Aufgabe: <L01_Zufallsgedicht>
+Name: <Lara Sophia Elisabeth Halmosi>
+Matrikel: <27>
+Datum: <13.10.2022>
+Quellen: <Kommilitonis mit denen Du zusammengearbeitet hast oder von denen Du dich inspirieren ließest>
+*/
+//Array//
+
+let subjekt: string [] = [  "Harry ",  "Hermine ",  "Ron ",  "Hagrid ",  "Snape ",  "Dumbledore "];
+let prädikat: string [] = [  "braut, liebt, studiert, hasst, zaubert, zerstört"];
+let objekt: string [] = [  "Zaubertränke, den Grimm, Lupin, Hogwarts, die Karte des Rumtreibers, Dementoren "];
+
+//for schleife//
+for (num=1; num<=10; num++)  {  
+    console.log(num); 
+    }
