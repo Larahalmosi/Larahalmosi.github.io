152c268441a5fc4d9fb564c27a1c3b4b33ead8d8
@y0urv39et4bles <101884905+Larahalmosi@users.noreply.github.com>
2022-10-20 12:43:46 +0200

    ++

--- A01_RandomPoem/poem.js
+++ A01_RandomPoem/poem.js
@@ -7,9 +7,9 @@ Datum: <13.10.2022>
 Quellen: <Kommilitonis mit denen Du zusammengearbeitet hast oder von denen Du dich inspirieren ließest>
 */
 //Array//
-let subjekt = ["Harry ", "Hermine ", "Ron ", "Hagrid ", "Snape ", "Dumbledore "];
-let prädikat = ["braut, liebt, studiert, hasst, zaubert, zerstört"];
-let objekt = ["Zaubertränke, den Grimm, Lupin, Hogwarts, die Karte des Rumtreibers, Dementoren "];
+let subject = ["Harry ", "Hermine ", "Ron ", "Hagrid ", "Snape ", "Dumbledore "];
+let predicate = ["braut, liebt, studiert, hasst, zaubert, zerstört"];
+let object = ["Zaubertränke, den Grimm, Lupin, Hogwarts, die Karte des Rumtreibers, Dementoren "];
 //for schleife//
 console.log("hello");
 for ((let); index; )
