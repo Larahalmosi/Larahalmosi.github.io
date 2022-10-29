44a5f09178ded2ef6b199cff57c07e5dd69378d9
@y0urv39et4bles <101884905+Larahalmosi@users.noreply.github.com>
2022-10-29 17:54:34 +0200

    korrektur
    neuw

--- A02_EventInspector/eventinspector.js
+++ A02_EventInspector/eventinspector.js
@@ -3,38 +3,60 @@
 Aufgabe: <L02_Eventinspector>
 Name: <Lara Sophia Elisabeth Halmosi>
 Matrikel: <271343>
-Datum: <22.10.2022>
-Quellen: <Kommilitonis mit denen Du zusammengearbeitet hast oder von denen Du dich inspirieren ließest>
+Datum: <29.10.2022>
+Quellen: inspiriert von Alexander Holstein und Ann-Kathrin Haas
 */
 var EventInspector;
 (function (EventInspector) {
     /*listener*/
     window.addEventListener("load", handleLoad);
-    window.addEventListener("mousemove", setInfoBox);
-    window.addEventListener("click", loginfo);
-    window.addEventListener("key up", loginfo);
-    let xPosition;
-    let yPosition;
     /*function*/
-    function handleLoad() {
+    function handleLoad(_event) {
+        let div0 = document.querySelector("div0");
+        let div1 = document.querySelector("div1");
+        let button = document.querySelector("button");
         /*mousemove*/
         document.addEventListener("mousemove", setInfoBox);
         /*click*/
         document.addEventListener("click", loginfo);
-        body.addEventListener("click", loginfo);
-        div.addEventListener("click", loginfo);
+        document.body.addEventListener("click", loginfo);
+        div0.addEventListener("click", loginfo);
+        div1.addEventListener("click", loginfo);
         /*key up*/
         document.addEventListener("key up", loginfo);
-        body.addEventListener("key up", loginfo);
-        div.addEventListener("key up", loginfo);
+        document.body.addEventListener("key up", loginfo);
+        div0.addEventListener("key up", loginfo);
+        div1.addEventListener("key up", loginfo);
+        /*customEvent listener*/
+        button.addEventListener("click", createcustomEvent);
+        document.addEventListener("CustomEvent", logcustomEvent);
     }
-    function setInfoBox() {
-        xPosition = _event.x;
-        yPosition = _event.y;
+    function setInfoBox(_event) {
+        let span = document.querySelector("span");
+        console.log(span);
+        let posX = `${_event.clientX}`;
+        let posY = `${_event.clientY}`;
+        span.innerHTML = `X: ${posX},<br> Y: ${posY},<br> Target: ${_event.target}`;
+        let offsetX = _event.clientX + 15;
+        let offsetY = _event.clientY + 15;
+        span.style.left = `${offsetX}px`;
+        span.style.top = `${offsetY}px`;
     }
-    function loginfo() {
+    function loginfo(_event) {
+        console.log("Type" + _event);
         console.log("Current Target:" + _event.currentTarget);
-        console.log("Target: " + _event.target);
+        console.log("Target" + _event.target);
+        console.log(_event);
+    }
+    /*customEvent is getting created*/
+    function createcustomEvent(_event) {
+        let event = new CustomEvent("CustomEvent", { bubbles: true });
+        let button = document.querySelector("button");
+        button.dispatchEvent(event);
+    }
+    /*customEvent in browser console*/
+    function logcustomEvent(_event) {
+        console.log(_event);
     }
 })(EventInspector || (EventInspector = {}));
 //# sourceMappingURL=eventinspector.js.map
--- A02_EventInspector/eventinspector.ts
+++ A02_EventInspector/eventinspector.ts
@@ -2,44 +2,78 @@
 Aufgabe: <L02_Eventinspector>
 Name: <Lara Sophia Elisabeth Halmosi>
 Matrikel: <271343>
-Datum: <22.10.2022>
-Quellen: <Kommilitonis mit denen Du zusammengearbeitet hast oder von denen Du dich inspirieren ließest>
+Datum: <29.10.2022>
+Quellen: inspiriert von Alexander Holstein und Ann-Kathrin Haas
 */
+
 namespace EventInspector {
+
 /*listener*/
 window.addEventListener("load", handleLoad);
-window.addEventListener("mousemove", setInfoBox);
-window.addEventListener("click", loginfo);
-window.addEventListener("key up", loginfo);
 
-
-let xPosition: number;
-let yPosition: number;
 /*function*/
-function handleLoad() {
-    /*mousemove*/
+function handleLoad( _event: Event ): void {
+
+    let div0: HTMLElement = <HTMLElement>document.querySelector("div0");
+    let div1: HTMLElement = <HTMLElement>document.querySelector("div1");
+    let button: HTMLElement = <HTMLElement>document.querySelector("button");
+/*mousemove*/
     document.addEventListener("mousemove", setInfoBox );
 
-     /*click*/
+ /*click*/
     document.addEventListener("click", loginfo );
-    body.addEventListener("click", loginfo );
-    div.addEventListener("click", loginfo );
+    document.body.addEventListener("click", loginfo );
+    div0.addEventListener("click", loginfo );
+    div1.addEventListener("click", loginfo );
 
- /*key up*/
+/*key up*/
     document.addEventListener("key up", loginfo);
-    body.addEventListener("key up", loginfo);
-    div.addEventListener("key up", loginfo);
+    document.body.addEventListener("key up", loginfo);
+    div0.addEventListener("key up", loginfo);
+    div1.addEventListener("key up", loginfo);
+
+/*customEvent listener*/
+    button.addEventListener("click", createcustomEvent);
+    document.addEventListener("CustomEvent", logcustomEvent);
+
 }
 
 
-function setInfoBox() {
-    xPosition = _event.x;
-    yPosition = _event.y;
+function setInfoBox( _event: MouseEvent): void {
+
+    let span: HTMLSpanElement = <HTMLSpanElement>document.querySelector("span");
+    console.log(span);
+
+    let posX: string = `${ _event.clientX }`;
+    let posY: string = `${ _event.clientY }`;
+    span.innerHTML = `X: ${posX},<br> Y: ${posY},<br> Target: ${_event.target}`;
+
+    let offsetX: number = _event.clientX + 15;
+    let offsetY: number = _event.clientY + 15;
+    span.style.left = `${offsetX}px`;
+    span.style.top = `${offsetY}px`;
 
 }
 
-function loginfo() {
+function loginfo(_event: Event ): void {
+
+console.log("Type" + _event);
 console.log("Current Target:" + _event.currentTarget);
-console.log("Target: " + _event.target);
+console.log("Target" + _event.target);
+console.log (_event);
+}
+
+/*customEvent is getting created*/
+
+function createcustomEvent(_event: Event): void {
+let event: CustomEvent = new CustomEvent("CustomEvent", {bubbles: true});
+let button: HTMLElement = <HTMLElement>document.querySelector("button");
+button.dispatchEvent(event);
+}
+
+ /*customEvent in browser console*/
+
+function logcustomEvent(_event: Event): void {
+    console.log (_event);
 }
 }
