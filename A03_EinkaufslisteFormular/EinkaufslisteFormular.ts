fdc1d7d9bba470e8666bd6142f2fe97b4d330c9f
@y0urv39et4bles <101884905+Larahalmosi@users.noreply.github.com>
2022-10-29 19:42:34 +0200

    ++

---
+++ A03_EinkaufslisteFormular/EinkaufslisteFormular.ts
@@ -0,0 +1,28 @@
+/*
+Aufgabe: <L01_Zufallsgedicht>
+Name: <Lara Sophia Elisabeth Halmosi>
+Matrikel: <271343>
+Datum: <22.10.2022>
+Quellen: <Kommilitonis mit denen Du zusammengearbeitet hast oder von denen Du dich inspirieren ließest>
+*/
+
+namespace Einkaufsliste_Formular{
+window.addEventListener("load",)
+
+/*function list*/
+
+function list():void{
+let
+
+}
+
+
+
+
+
+
+
+
+
+
+}
