2f72a16a9cdd39816fa00f150d4753926d4f3f76
@y0urv39et4bles <lara.sophia.elisabeth.halmosi@hs-furtwangen.de>
2022-10-29 22:13:59 +0200

    new

--- A03_EinkaufslisteFormular/EinkaufslisteFormular.ts
+++ A03_EinkaufslisteFormular/EinkaufslisteFormular.ts
@@ -1,22 +1,53 @@
 /*
-Aufgabe: <L01_Zufallsgedicht>
+Aufgabe: <L03_EinkaufslisteFormular>
 Name: <Lara Sophia Elisabeth Halmosi>
 Matrikel: <271343>
 Datum: <22.10.2022>
-Quellen: <Kommilitonis mit denen Du zusammengearbeitet hast oder von denen Du dich inspirieren ließest>
+Quellen: <inspiriert von Alina Stumpf>
 */
 
 namespace Einkaufsliste_Formular{
-window.addEventListener("load",)
+
+/*listener*/
+
+window.addEventListener("load",list)
 
 /*function list*/
 
 function list():void{
-let
+    let addButton: Element = document.querySelector("#add");
+    let deleteButton: Element = document.querySelector("#trash");
+    let checkboxButton: Element = document.querySelector(".checkbox");
+    let editButton: Element=document.querySelector("#edit")
+
+    addButton.addEventListener("click", add);
+    deleteButton.addEventListener("click", trash);
+    checkboxButton.addEventListener("click", check);
+    editButton.addEventListener("click", edit)
 
 }
 
 
+/*console log item hinzufügen*/
+function add():void{
+
+    console.log("fügt etwas hinzu");
+}
+/*console log item wegschmeißen */
+function trash():void{
+
+    console.log("weg damit");
+}
+/*console log item eingekauft */
+function check():void{
+
+    console.log("item eingekauft");
+}
+/*console log bearbeiten */
+function edit():void{
+
+    console.log("falls sich etwas ändert");
+}
 
 
 
