ce8613e9984dab740b4d42ed504825c75a3d964f
@y0urv39et4bles <101884905+Larahalmosi@users.noreply.github.com>
2022-10-30 18:55:33 +0100

    add
    ++

---
+++ A03_EinkaufslisteFormular/EinkaufslisteFormular.js
@@ -0,0 +1,41 @@
+"use strict";
+/*
+Aufgabe: <L03_EinkaufslisteFormular>
+Name: <Lara Sophia Elisabeth Halmosi>
+Matrikel: <271343>
+Datum: <22.10.2022>
+Quellen: <inspiriert von Alina Stumpf>
+*/
+var Einkaufsliste_Formular;
+(function (Einkaufsliste_Formular) {
+    /*listener*/
+    window.addEventListener("load", list);
+    /*function list*/
+    function list() {
+        let addButton = document.querySelector("#add");
+        let deleteButton = document.querySelector("#trash");
+        let checkboxButton = document.querySelector(".checkbox");
+        let editButton = document.querySelector("#edit");
+        addButton.addEventListener("click", add);
+        deleteButton.addEventListener("click", trash);
+        checkboxButton.addEventListener("click", check);
+        editButton.addEventListener("click", edit);
+    }
+    /*console log item hinzufügen*/
+    function add() {
+        console.log("fügt etwas hinzu");
+    }
+    /*console log item wegschmeißen */
+    function trash() {
+        console.log("weg damit");
+    }
+    /*console log item eingekauft */
+    function check() {
+        console.log("item eingekauft");
+    }
+    /*console log bearbeiten */
+    function edit() {
+        console.log("falls sich etwas ändert");
+    }
+})(Einkaufsliste_Formular || (Einkaufsliste_Formular = {}));
+//# sourceMappingURL=EinkaufslisteFormular.js.map
--- A03_EinkaufslisteFormular/EinkaufslisteFormular.ts
+++ A03_EinkaufslisteFormular/EinkaufslisteFormular.ts
@@ -6,45 +6,45 @@ Datum: <22.10.2022>
 Quellen: <inspiriert von Alina Stumpf>
 */
 
-namespace Einkaufsliste_Formular{
+namespace Einkaufsliste_Formular {
 
 /*listener*/
 
-window.addEventListener("load",list)
+window.addEventListener("load", list);
 
 /*function list*/
 
-function list():void{
+function list(): void {
     let addButton: Element = document.querySelector("#add");
     let deleteButton: Element = document.querySelector("#trash");
     let checkboxButton: Element = document.querySelector(".checkbox");
-    let editButton: Element=document.querySelector("#edit")
+    let editButton: Element = document.querySelector("#edit");
 
     addButton.addEventListener("click", add);
     deleteButton.addEventListener("click", trash);
     checkboxButton.addEventListener("click", check);
-    editButton.addEventListener("click", edit)
+    editButton.addEventListener("click", edit);
 
 }
 
 
 /*console log item hinzufügen*/
-function add():void{
+function add(): void {
 
     console.log("fügt etwas hinzu");
 }
 /*console log item wegschmeißen */
-function trash():void{
+function trash(): void {
 
     console.log("weg damit");
 }
 /*console log item eingekauft */
-function check():void{
+function check(): void {
 
     console.log("item eingekauft");
 }
 /*console log bearbeiten */
-function edit():void{
+function edit(): void {
 
     console.log("falls sich etwas ändert");
 }
